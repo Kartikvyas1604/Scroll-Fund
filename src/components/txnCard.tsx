339cfd87506efacf51d949c7999e6b4a5350a7b1
thakoraarya <aarya191102@gmail.com>
2023-11-07 16:09:56 +0530

    updated dashboard
    changed from card list view to table view

--- src/components/txnCard.tsx
+++ src/components/txnCard.tsx
@@ -1,34 +1,38 @@
-import { Box, Flex, Stack, StackDivider, Text } from "@chakra-ui/react";
 import React from "react";
+import {Table, TableContainer, Tbody, Td, Th, Thead, Tr,} from '@chakra-ui/react'
 
 interface txnDetails {
-  amount: number;
-  message: string;
-  senderAddress: string;
-  timestamp: number;
+    amount: number;
+    message: string;
+    senderAddress: string;
+    timestamp: number;
 }
 
-export default function TxnCard({ txn }: { txn: txnDetails[] }) {
-  return (
-    <>
-      {txn.map((item, index) => (
-        <Stack divider={<StackDivider bgColor={"#FF684B"}/>} key="" spacing="2">
-          <Box>
-            <Flex alignItems={"center"}>
-              <Box>
-                <Text fontSize={"md"} color={"black"}><b>From:</b> {item.senderAddress}</Text>
-              </Box>
-            </Flex>
-            <Box justifyContent={"end"} alignContent={"end"} color={"black"}>
-              <b>Amount: </b> {item.amount} Scroll
-            </Box>
-            <Text pt="2" fontSize="md" color={"black"}>
-            <b>Message: </b>  {item.message}
-            </Text>
-          </Box>
-          <br></br>
-        </Stack>
-      ))}
-    </>
-  );
+export default function TxnCard({txn}: { txn: txnDetails[] }) {
+    return (
+        <>
+
+
+            <TableContainer>
+                <Table variant='simple'>
+                    <Thead>
+                        <Tr>
+                            <Th>From</Th>
+                            <Th>Message</Th>
+                            <Th isNumeric>Amount</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {txn.map((item, index) => (<Tr>
+                            <Td>{item.senderAddress}</Td>
+                            <Td>  {item.message}</Td>
+                            <Td isNumeric>{item.amount} Scroll</Td>
+                        </Tr>))}
+                    </Tbody>
+
+                </Table>
+            </TableContainer>
+
+        </>
+    );
 }
